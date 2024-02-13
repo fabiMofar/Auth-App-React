468dfc1739987de4596af3ee06b49cee2044ee68
Fabi Mofar <fabian_fj1997@hotmail.com>
2024-02-13 21:39:56 +0100

    create signup Route

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,33 @@
 import express from 'express'
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import userRoute from "./routes/user.route.js";
-import authRoute from "./routes/auth.route.js";
+import userRoute from './routes/user.route.js'
+import authRoute from './routes/auth.route.js'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 
-
-mongoose.connect(process.env.MONGODB_ADDRESS)
+mongoose
+    .connect(process.env.MONGO_DB_ADDRESS)
     .then(() => {
-    console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB')
     })
     .catch((err) => {
-        console.log(err);
-    });
-
+        console.log(err)
+    })
 
 const app = express()
 
-app.use('/api/user', userRoute);
-app.use('/api/auth', authRoute);
+app.use(express.json())
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.use('/api/user', userRoute)
+app.use('/api/auth', authRoute)
 
 app.listen(3000, () => {
     console.log('server on 3000')
